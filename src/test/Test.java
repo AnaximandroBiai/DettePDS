22f586d7141fd582bfcf5b20a2a87400c71d80b6
Anaximandro Biai <anaximandrobiai@gmail.com>
2019-04-11 23:22:01 +0200

    add TU

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,7 +3,14 @@ package test;
 
 
 
+import java.sql.Connection;
+import java.util.Collection;
+
 import connection.ConnectionPool;
+import dao.StockDAO;
+import dao.TurnoverDAO;
+import pojo.Stock;
+import pojo.Turnover;
 
 
 public class Test {
@@ -37,6 +44,34 @@ public class Test {
 	public boolean poolTest3() {
 		ConnectionPool p = new ConnectionPool(0);
 		if(p.getConnection() == null) {
+			System.out.println("Success\n");
+			return true;
+		}
+		else {
+			System.out.println("Failure\n");
+			return false;
+		}
+	}
+	
+	public boolean turnoverTest() {
+		ConnectionPool p = new ConnectionPool(1);
+		TurnoverDAO testDao = new TurnoverDAO(p.getConnection());
+		Collection<Turnover> testTurnO= testDao.find("Sport");
+		if(testTurnO != null) {
+			System.out.println("Success\n");
+			return true;
+		}
+		else {
+			System.out.println("Failure\n");
+			return false;
+		}
+	}
+	
+	public boolean clientReturnTest() {
+		ConnectionPool p = new ConnectionPool(1);
+		StockDAO testDao = new StockDAO(p.getConnection());
+		Collection<Stock> testClientR= testDao.find("FastFood");
+		if(testClientR != null) {
 			System.out.println("Success\n");
 			return true;
 		}
